22147cdba70a40451c284d60fd42122e7a142607
fueripe-desu <fueripedesu2020@outlook.com>
2024-07-05 02:59:08 -0300

    feat: create Logo svelte component

---
+++ src/components/Logo.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let leftPadding = '0px';
+  export let topPadding = '0px';
+
+  $: customStyle = `padding-left: ${leftPadding};padding-top: ${topPadding};`;
+</script>
+
+<div style={customStyle}>
+  <img src='/logo.svg' alt='Logo' />
+</div>

be5aa671cf9647103944eca82c79bb09aaab7e73
fueripe-desu <fueripedesu2020@outlook.com>
2024-07-13 21:04:03 -0300

    feat: add 'zindex' prop to specify the z-index of the Logo component

--- src/components/modules/Logo.svelte
+++ src/components/modules/Logo.svelte
@@ -1,7 +1,10 @@
 <script>
   export let url = "#";
+  export let zindex = "0";
+
+  $: customStyle = `z-index: ${zindex};`;
 </script>
 
-<a href={url}>
+<a href={url} style={customStyle}>
   <img src='/logo.svg' alt='Logo' />
 </a>
